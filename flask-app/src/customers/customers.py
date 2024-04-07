f54a8fed8aee8ed47a7a21594178debac3dd3c07
singhania-sa <147664492+singhania-sa@users.noreply.github.com>
2024-04-07 19:05:21 -0400

    added an endpoint

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -9,7 +9,7 @@ customers = Blueprint('customers', __name__)
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
+    cursor.execute('select id, company, last_name,\
         first_name, job_title, business_phone from customers')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -20,6 +20,22 @@ def get_customers():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Update a customer's details for customer with particular userID
+@customers.route('/modify-customer/<userID>', methods=['PUT'])
+def update_customer(userID, firstName):
+     # Constructing the query
+    query = 'update customers set first_name = "' + firstName 
+    query += '" where id = {0}'.format(userID)
+    current_app.logger.info(query)
+
+    #executing and committing the update statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
+    
 
 # Get customer detail for customer with particular userID
 @customers.route('/customers/<userID>', methods=['GET'])
